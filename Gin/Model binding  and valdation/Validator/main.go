45a29cac0fd10b14e67afb228d9e54e6a1f81928
Lucky shukla <68693212+Luckyshukla@users.noreply.github.com>
2021-09-07 19:51:31 +0530

    Add files via upload

--- Gin/Model binding  and valdation/Validator/main.go
+++ Gin/Model binding  and valdation/Validator/main.go
@@ -1,93 +1,35 @@
-package main
+package  main
 import (
-	"fmt"
-	"github.com/go-playground/validator/v10"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
-type user struct{
-	firstname string `validate:"required"`
-	lastname string `validate:"required"`
-	age string `validate:"required"`
-	Email string `validate:"required"`
-	favouriteColor string `validate:"string"`
-	Address []*Address `validate:"required,dive,required"`
+
+var secrets = gin.H{
+	"foo":    gin.H{"email": "foo@bar.com", "phone": "123433"},
+	"austin": gin.H{"email": "austin@example.com", "phone": "666"},
+	"lena":   gin.H{"email": "lena@guapa.com", "phone": "523443"},
 }
-type Address struct{
-	Street string `validate:"required"`
-	City string `validate:"required"`
-	Planet string `validate:"required"`
-	Phone string `validate:"required"`
-}
-// use a single instance of Validate, it caches struct info
-var validate *validator.Validate
+
 func main() {
+	r := gin.Default()
 
-	validate = validator.New()
 
-	validateStruct()
-	validateVariable()
-}
+	authorized := r.Group("/admin", gin.BasicAuth(gin.Accounts{
+		//"foo":    "bar",
+		"austin": "1234",
+		"lena":   "hello2",
+		"manu":   "4321",
+	}))
 
-func validateStruct() {
-
-	address := &Address{
-		Street: "Eavesdown Docks",
-		Planet: "Persphone",
-		Phone:  "none",
-	}
-
-	user := &user{
-		FirstName:      "Badger",
-		LastName:       "Smith",
-		Age:            135,
-		Email:          "Badger.Smith@gmail.com",
-		FavouriteColor: "#000-",
-		Addresses:      []*Address{address},
-	}
-
-	// returns nil or ValidationErrors ( []FieldError )
-	err := validate.Struct(user)
-	if err != nil {
-
-		// this check is only needed when your code could produce
-		// an invalid value for validation such as interface with nil
-		// value most including myself do not usually have code like this.
-		if _, ok := err.(*validator.InvalidValidationError); ok {
-			fmt.Println(err)
-			return
+	// hit "dmlocalhost:8080/ain/secrets
+	authorized.GET("/secrets", func(c *gin.Context) {
+		// get user, it was set by the BasicAuth middleware
+		user := c.MustGet(gin.AuthUserKey).(string)
+		if secret, ok := secrets[user]; ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
 		}
-
-		for _, err := range err.(validator.ValidationErrors) {
-
-			fmt.Println(err.Namespace())
-			fmt.Println(err.Field())
-			fmt.Println(err.StructNamespace())
-			fmt.Println(err.StructField())
-			fmt.Println(err.Tag())
-			fmt.Println(err.ActualTag())
-			fmt.Println(err.Kind())
-			fmt.Println(err.Type())
-			fmt.Println(err.Value())
-			fmt.Println(err.Param())
-			fmt.Println()
-		}
-
-		// from here you can create your own error messages in whatever language you wish
-		return
-	}
-
-	// save user to database
-}
-
-func validateVariable() {
-
-	myEmail := "joeybloggs.gmail.com"
-
-	errs := validate.Var(myEmail, "required,email")
-
-	if errs != nil {
-		fmt.Println(errs) // output: Key: "" Error:Field validation for "" failed on the "email" tag
-		return
-	}
-
-	// email ok, move on
+	})
+	r.Run(":8080")
 }
